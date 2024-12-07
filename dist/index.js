9457c2d9752c359a41c12874673e994b30408ca3
patiabhishek123 <abhishekcloud2003@gmail.com>
2024-12-07 13:06:31 +0530

    init

---
+++ dist/index.js
@@ -0,0 +1,41 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = require("ws");
+const wss = new ws_1.WebSocketServer({ port: 8080 });
+let socketArray = [];
+console.log("not yet connected");
+wss.on('connection', (socket) => {
+    console.log("connected for now");
+    socket.on("message", (message) => {
+        // @ts-ignore
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type === 'join') {
+            console.log("user wants to join");
+            socketArray.push({
+                socket,
+                RoomId: parsedMessage.payload.RoomId
+            });
+            console.log("user joined");
+        }
+        let currentUserRoomId = null;
+        if (parsedMessage.type === 'chat') {
+            console.log("user wants to chat");
+            console.log("Room joined is: " + parsedMessage.payload.RoomId);
+            let currentUserRoomId = null;
+            for (let i = 0; i < socketArray.length; i++) {
+                if (socketArray[i].socket === socket) {
+                    currentUserRoomId = socketArray[i].RoomId;
+                }
+            }
+            for (let j = 0; j < socketArray.length; j++) {
+                if (socketArray[j].RoomId == currentUserRoomId) {
+                    socketArray[j].socket.send(parsedMessage.payload.message);
+                }
+            }
+        }
+    });
+    // socket.on("disconnect",()=>
+    // {
+    //   socketArray=socketArray.filter((x.socket)=>x.socket!=parsedMessage.socket);
+    // })
+});
